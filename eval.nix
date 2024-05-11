c7e345c9d33f3e2d5591df8a24c1bd19f063d749
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-05-11 18:42:06 +0200

    integrate system config directly into module

--- eval.nix
+++ eval.nix
@@ -1,8 +1,12 @@
 { pkgs ? import <nixpkgs> {}
 , config ? {
   node1 = {
-    nixosConfig = {};
-    nspawnConfig = {};
+    system = "aarch64-linux";
+    networking.bridge = "br-kv";
+    files = [ { path  ="/home"; } ];
+    # nixosConfig = { networking.interfaces.host0.ipv4.addresses = [{address="192.168.10.1"; prefixLength=24;}]; };
+    networking.address = [{address="192.168.10.1"; prefixLength=24;}];
+    networking.ports = [ "2000" ];
   };
 }
 } :
@@ -11,47 +15,47 @@ let
   inherit (pkgs.lib)
   evalModules
   mapAttrs
+  optionalString
   ;
 
-  evalNode = name: node:
-    import (pkgs.path + "/nixos/lib/eval-config.nix") {
-      modules = [
-        node
-        (import ./container.nix name)
-      ];
-    };
-
   systemClosure = node:
-    node.config.system.build.toplevel;
+    node.nixosConfig.system.build.toplevel;
 
-  evalNspawn = nspawn: evalModules {
+  evalConfig = name: node: evalModules {
     modules = [
       { _module.args = {
-         inherit pkgs;
+         inherit name pkgs;
          inherit (pkgs) lib;
         };
       }
       (import ./module.nix)
+      node
     ];
   };
 
 in
   mapAttrs (name: node:
     let
-      evaluatedNode = evalNode name node.nixosConfig;
-      system = systemClosure evaluatedNode;
-      nspawn = evalNspawn node.nspawnConfig;
-      rootPath = nspawn.config.root + name;
+      system = systemClosure nodeConfig;
+      nodeConfig = (evalConfig name node).config;
+      rootPath = nodeConfig.root;
+
+      containerScript = pkgs.writeShellScript "container-${name}"
+      ''
+        # create and clean root dir
+        ${pkgs.coreutils}/bin/mkdir -p ${system}
+
+        systemd-nspawn --private-network --private-users=pick --resolv-conf=copy-host --bind-ro /nix/store \
+            ${nodeConfig.commandLine} \
+            -M "${nodeConfig.prefix + name}" \
+            -D ${rootPath} ${system}/init
+
+        ${pkgs.coreutils}/bin/rm -r ${system}
+      '';
     in
-    pkgs.writeShellScript "launch-${name}"
-    ''
-      ${pkgs.coreutils}/bin/mkdir -p ${rootPath}
-
-      ${pkgs.coreutils}/bin/ls -ld ${rootPath}
-      systemd-run --working-directory=${rootPath} \
-      systemd-nspawn --private-network --private-users=pick --resolv-conf=copy-host --bind-ro /nix \
-          ${nspawn.config.commandLine} -M "${nspawn.config.prefix + name}" -D ${rootPath} ${system}/init
-
+    pkgs.writeScript "run-${name}" ''
+      systemd-run ${optionalString (nodeConfig.host != null) "-H ${nodeConfig.host}"} \
+          --working-directory=${rootPath} ${containerScript}
     ''
   ) config
 
--- module.nix
+++ module.nix
@@ -1,16 +1,26 @@
-{ pkgs, lib, config, ... } :
+{ pkgs, lib, config, name, ... } :
 
 let
   inherit (lib)
   types
   mkOption
+  mkDefault
+  mkIf
+  concatStrings
   concatStringsSep
+  optionalString
   ;
 
 in {
   options = {
-    name = mkOption {
+    host = mkOption {
+      type = with types; nullOr str;
+      default = null;
+    };
 
+    system = mkOption {
+      type = types.str;
+      default = pkgs.system;
     };
 
     prefix = mkOption {
@@ -20,14 +30,32 @@ in {
 
     root = mkOption {
       type = types.str;
-      default = "/run/container/${config.prefix}";
+      default = "/run/container/${config.prefix}${name}";
     };
 
     networking = {
-      interfaces = mkOption {
+      extraConfig = mkOption {
         type = types.str;
         description = "Network interface definitions";
         default = "";
+      };
+
+      address = mkOption {
+        description = "IPv4 address for host0 interface";
+        default = null;
+        type = with types; listOf attrs;
+      };
+
+      ports = mkOption {
+        type = with types; listOf (oneOf [str port]);
+        description = "List of port forwardings";
+        default = [];
+      };
+
+      bridge = mkOption {
+        type = with types; nullOr str;
+        default = null;
+        description = "Network bridge interface";
       };
     };
 
@@ -47,20 +75,59 @@ in {
       }));
     };
 
-    commandLine = mkOption { type = types.str; };
+    nixosConfig = mkOption {
+      description = lib.mdDoc ''
+        A specification of the desired configuration of this container, as a NixOS module.
+      '';
+      default = {};
+      type = lib.mkOptionType {
+        name = "Toplevel NixOS config";
+        merge = loc: defs: (import "${toString pkgs.path}/nixos/lib/eval-config.nix" {
+          modules =
+            let
+              extraConfig = { options, ... }: {
+                _file = "module at ${__curPos.file}:${toString __curPos.line}";
+                config = {
+                  # nixpkgs = if options.nixpkgs?hostPlatform && host.options.nixpkgs.hostPlatform.isDefined
+                  #           then { inherit (host.config.nixpkgs) hostPlatform; }
+                  #           else { inherit (host.config.nixpkgs) localSystem; }
+                  # ;
+                  nixpkgs = {
+                    # inherit pkgs;
+                    buildPlatform.system = builtins.currentSystem;
+                    hostPlatform.system = config.system;
+                  };
+                  boot.isContainer = true;
+                  networking.hostName = mkDefault name;
+                  networking.useDHCP = mkDefault false;
+                  networking.interfaces.host0.ipv4.addresses = mkIf (config.networking.address != null) config.networking.address;
+                };
+              };
+            in [ extraConfig ] ++ (map (x: x.value) defs);
+          prefix = [ "nixosConfig" ];
+          # inherit (config) specialArgs;
+
+          # The system is inherited from the host above.
+          # Set it to null, to remove the "legacy" entrypoint's non-hermetic default.
+          system = null;
+        }).config;
+      };
+    };
+
+    # internal options
+    commandLine = mkOption { type = types.str; default = null; };
   };
 
   config = {
-    commandLine = config.networking.interfaces + " "
+    commandLine = config.networking.extraConfig + " "
       + (concatStringsSep " " (map (f:
         (if f.ro then "--bind-ro " else "--bind ") + f.path
-      ) config.files ));
+      ) config.files ))
+      + (concatStrings (map (p:
+        " -p ${toString p}"
+      ) config.networking.ports ))
+      + optionalString (config.networking.bridge != null) " --network-bridge=${config.networking.bridge}"
+      ;
   };
 }
-
-# {
-#   node1 = {
-#     nspawnConfig=
-#     nixosConfig
-#   };
 
