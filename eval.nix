01c765c48a98be4d8efc2da1a8448577805490ce
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-05-11 21:58:10 +0200

    update eval.nix

--- eval.nix
+++ eval.nix
@@ -1,12 +1,15 @@
 { pkgs ? import <nixpkgs> {}
 , config ? {
   node1 = {
-    system = "aarch64-linux";
+    # system = "aarch64-linux";
     networking.bridge = "br-kv";
-    files = [ { path  ="/home"; } ];
-    # nixosConfig = { networking.interfaces.host0.ipv4.addresses = [{address="192.168.10.1"; prefixLength=24;}]; };
+    files = [ { path  ="/tmp/share"; ro= false; } ];
     networking.address = [{address="192.168.10.1"; prefixLength=24;}];
     networking.ports = [ "2000" ];
+  };
+  node2 = {
+    networking.bridge = "br-kv";
+    networking.address = [{address="192.168.10.2"; prefixLength=24;}];
   };
 }
 } :
@@ -14,13 +17,16 @@
 let
   inherit (pkgs.lib)
   evalModules
+  attrsToList
   mapAttrs
   optionalString
+  concatStringsSep
   ;
 
   systemClosure = node:
     node.nixosConfig.system.build.toplevel;
 
+  # Eval node module
   evalConfig = name: node: evalModules {
     modules = [
       { _module.args = {
@@ -33,8 +39,8 @@ let
     ];
   };
 
-in
-  mapAttrs (name: node:
+  # Generate container runner script
+  nodeRunners = mapAttrs (name: node:
     let
       system = systemClosure nodeConfig;
       nodeConfig = (evalConfig name node).config;
@@ -43,19 +49,25 @@ in
       containerScript = pkgs.writeShellScript "container-${name}"
       ''
         # create and clean root dir
-        ${pkgs.coreutils}/bin/mkdir -p ${system}
+        ${pkgs.coreutils}/bin/mkdir -p ${rootPath}
 
         systemd-nspawn --private-network --private-users=pick --resolv-conf=copy-host --bind-ro /nix/store \
             ${nodeConfig.commandLine} \
             -M "${nodeConfig.prefix + name}" \
             -D ${rootPath} ${system}/init
 
-        ${pkgs.coreutils}/bin/rm -r ${system}
+        ${pkgs.coreutils}/bin/rm -r ${rootPath}
       '';
     in
     pkgs.writeScript "run-${name}" ''
       systemd-run ${optionalString (nodeConfig.host != null) "-H ${nodeConfig.host}"} \
-          --working-directory=${rootPath} ${containerScript}
+          ${containerScript}
     ''
-  ) config
+  ) config;
+
+in {
+  all = pkgs.writeShellScript "run-all"
+    (concatStringsSep "\n" (map (x: x.value) (attrsToList nodeRunners)));
+  } // nodeRunners
+
 
