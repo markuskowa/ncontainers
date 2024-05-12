c27a202e65a84e7d52c352e49b8bcbf727d35554
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-05-12 22:12:12 +0200

    add test to flake

--- flake.nix
+++ flake.nix
@@ -10,16 +10,41 @@
       nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
-      ] (system: function nixpkgs.legacyPackages.${system});
+      ] (system: function nixpkgs.legacyPackages.${system} system);
 
   in
   {
 
     lib.eval = import ./eval.nix;
 
-    packages = forAllSystems (pkgs: {
-
-    });
+    hydraJobs = forAllSystems (pkgs: system: let
+      node1Addr = "192.168.10.1";
+      brAddr = "192.168.10.254";
+      nodeRunner = self.lib.eval {
+        inherit pkgs system;
+        config = {
+          node1 = {
+            networking.bridge = "br-kv";
+            networking.address = [{address="${node1Addr}"; prefixLength=24;}];
+          };
+        };
+      };
+    in  {
+        launchSingleNode = (pkgs.nixosTest {
+          name =  "Single node";
+          nodes.main = {
+            networking.bridges.br-kv.interfaces = [ ];
+            networking.interfaces.br-kv.ipv4.addresses = [{address="${brAddr}"; prefixLength=24;}];
+          };
+          testScript = ''
+            start_all()
+            main.wait_for_unit("multi-user.target")
+            main.succeed("${nodeRunner.node1}")
+            main.wait_until_succeeds("machinectl status kv-node1")
+            main.wait_until_succeeds("ping -c 1 ${node1Addr}")
+          '';
+        });
+      });
 
     templates.default = {
       path = ./template;
